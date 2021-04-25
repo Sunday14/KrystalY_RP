908c95a0a386292f36ea3d4cc3143c63171f8886
Sunday14 <18268878136@163.com>
2021-04-26 00:40:58 +0800

    Add files via upload
    Krystal Y

---
+++ Assets/KrystalY_RenderPipeline/KrystalYRenderPipeline.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class KrystalYRenderPipeline : RenderPipeline
+{
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        BeginFrameRendering(context, cameras);
+    }
+}
---
+++ Assets/KrystalY_RenderPipeline/KrystalYRenderPipelineAsset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/Krystal Y Render Pipeline")]
+public class KrystalYRenderPipelineAsset : RenderPipelineAsset 
+{
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new KrystalYRenderPipeline();
+    }
+
+}
