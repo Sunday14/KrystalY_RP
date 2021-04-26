7a279792a8d570774ae1406c398ad4f918e6045b
Sunday14 <18268878136@163.com>
2021-04-27 00:42:20 +0800

    0427-ScriptableRenderContext

--- Assets/KrystalY_RenderPipeline/KrystalYRenderPipeline.cs
+++ Assets/KrystalY_RenderPipeline/KrystalYRenderPipeline.cs
@@ -8,5 +8,16 @@ public class KrystalYRenderPipeline : RenderPipeline
     protected override void Render(ScriptableRenderContext context, Camera[] cameras)
     {
         BeginFrameRendering(context, cameras);
+        foreach(Camera  k_camera in cameras)
+        {
+            RenderSingleCamera(context,k_camera);
+        }
+    }
+
+    public static void RenderSingleCamera(ScriptableRenderContext context, Camera k_camera)
+    {
+        context.SetupCameraProperties(k_camera);
+        context.DrawSkybox(k_camera);
+        context.Submit();
     }
 }
