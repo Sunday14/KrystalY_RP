7493935c72c069dadafffcaba969f0b658e67d45
Sunday14 <18268878136@163.com>
2021-04-30 01:23:49 +0800

    add profiler samples

--- Assets/KrystalY_RenderPipeline/KrystalYRenderPipeline.cs
+++ Assets/KrystalY_RenderPipeline/KrystalYRenderPipeline.cs
@@ -5,19 +5,27 @@ using UnityEngine.Rendering;
 
 public class KrystalYRenderPipeline : RenderPipeline
 {
+    CommandBuffer buffer = new CommandBuffer {name = "Camera Renderer"};
     protected override void Render(ScriptableRenderContext context, Camera[] cameras)
     {
-        BeginFrameRendering(context, cameras);
+        // BeginFrameRendering(context, cameras);
         foreach(Camera  k_camera in cameras)
         {
             RenderSingleCamera(context,k_camera);
         }
     }
 
-    public static void RenderSingleCamera(ScriptableRenderContext context, Camera k_camera)
+
+    public void RenderSingleCamera(ScriptableRenderContext context, Camera k_camera)
     {
+        buffer.BeginSample("Camera Renderer");
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
         context.SetupCameraProperties(k_camera);
         context.DrawSkybox(k_camera);
+        buffer.EndSample("Camera Renderer"); 
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
         context.Submit();
     }
 }
